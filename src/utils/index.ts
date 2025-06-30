9f26a04fe2722d7eef02dd5fec6aade9649be806
Rosiris Romero <rosirisvrm@gmail.com>
2025-06-30 11:18:56 -0400

    Change CV file

--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export function updateCvFile() {
-    const isDark = document.documentElement.classList.contains('dark');
-    const cvLink = document.getElementById('cv-button');
-    
-    if (cvLink) {
-        cvLink.setAttribute('href', isDark ? 'public/Rosiris-Romero-CV-Green.pdf' : 'Rosiris-Romero-CV-Pink.pdf');
-    }
-}
